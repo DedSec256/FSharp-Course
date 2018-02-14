c8438278788304ee0f500e7c67a8335f435685f0
Alex Berezhnyh <berejnih.alex2011@yandex.ru>
2018-02-14 20:59:29 +0300

    1.4 added (degreesOfTwoGenerator)

--- FSharp first steps/SimpleMath.fs
+++ FSharp first steps/SimpleMath.fs
@@ -4,3 +4,10 @@ module SimpleMath =
 
     (* Task 1.1 *)
     let factorial n : int = [2..n] |> List.reduce (*) 
+
+    (* Task 1.4 *)
+    let degreesOfTwoGenerator n m = 
+    [
+        for i in n..n+m
+        -> 2.0 ** i
+    ]
