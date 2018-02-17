58f41df7dc89113aa064adce55764bb5f915b0bc
Alex Berezhnyh <berejnih.alex2011@yandex.ru>
2018-02-18 01:34:41 +0300

    Task 1.4 optimization

--- FSharp first steps/SimpleMath.fs
+++ FSharp first steps/SimpleMath.fs
@@ -6,10 +6,14 @@ module SimpleMath =
      let (!) n : int = [2..n] |> List.reduce (*) 
     
      (* Task 1.4 *)
-     (* TODO: optimization *)
-     let rec degreesOfTwoGenerator n m =
-        if (m < 0.0) then []
-        else degreesOfTwoGenerator n (m - 1.0) @ [2.0 ** (n + m)]
+     (* TODO: more optimization *)
+     let degreesOfTwoGenerator n m =
+        let rec accGenerator m acc = 
+            if (m < 0) then []
+            else acc :: accGenerator (m - 1) (acc * 2)
+
+        accGenerator m (pown 2 n)
+        
 
      (* Task 1.3 *)
      let reverse list = 
@@ -22,7 +26,7 @@ module SimpleMath =
         reverseSignature [] list
      
 
-    (* Task 1.2, modified - difficulty O(ln n) 
+    (* Task 1.2, modified - difficulty O(ln n)  
      * ---------------------------------------------
      * Introduction:
      * ---------------------------------------------
@@ -46,13 +50,13 @@ module SimpleMath =
         let matrixMuliply ((A : int list list), (B : int list list)) = 
             let A1 = A.Item 1
             let B1 = B.Item 1
-            let A01 = (A.Item 0).Item 1
-            let B01 = (B.Item 0).Item 1
-            let A11 = A1.Item 1
+            let A01 = (A.Item 0).Item 1 (*  A = (A00 A01), B = (B00 B01)  *)
+            let B01 = (B.Item 0).Item 1 (*      (A10 A11)      (B10 B11)  *)
+            let A11 = A1.Item 1     
             let B11 = B1.Item 1
             [
-                [ ( (A.Item 0).Item 0 * (B.Item 0).Item 0 ) + ( A01 * B1.Item 0 );
-                  ( (A.Item 0).Item 0 * B01 ) + ( A01 * A11 ) ];
+                [ ( (A.Item 0 ).Item 0 * (B.Item 0).Item 0 ) + ( A01 * B1.Item 0 );
+                  ( (A.Item 0 ).Item 0 * B01 ) + ( A01 * A11 ) ];
                 [ ( A1.Item 0 * (B.Item 0).Item 0 ) + ( A11 * B1.Item 0 );
                   ( A1.Item 0 * B01 ) + ( A11 * B11 ) ];
             ]
@@ -63,7 +67,7 @@ module SimpleMath =
             elif n % 2 = 0 then matrixMuliply (fastMatrixPow A (n / 2), fastMatrixPow A (n / 2))
             else matrixMuliply (fastMatrixPow A (n - 1), A)
         
-        ((fastMatrixPow [[0; 1];[1;1]] n).Item 1).Item 0
+        ((fastMatrixPow [[0; 1];[1;1]] n).Item 1).Item 0 
         
         
      
