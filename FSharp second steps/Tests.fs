1992a54e21483c5222b676d500f49d0a17348419
Alex Berezhnykh <berejnih.alex2011@yandex.ru>
2018-03-26 22:53:03 +0300

    Update Tests.fs

--- FSharp second steps/Tests.fs
+++ FSharp second steps/Tests.fs
@@ -1,20 +1,33 @@
-﻿open FSharp_second_steps.SimpleDataStructures
+open FSharp_second_steps.SimpleDataStructures
 open FSharp_second_steps
 open NUnit.Framework
 open FsUnit
+open FsCheck
 open System
 
 module Tests = 
 
 
         (* Task 3.1 tests *)
-        [<Test>]
-        let ``count for [] should equal 0`` () =
-            countEvenFilter [] |> should equal 0
+        (* Проверяем на корректность countEvenFilter *)
+        [<TestFixture>]
+        module ``Count even tests`` =
+            let testData () =
+                [
+                    TestCaseData([]).Returns(0)
+                    TestCaseData([1; 2; 3; -4; 0]).Returns(3)
+                    TestCaseData([8; 10; 12; -4; 0]).Returns(5)
+                ]
+            [<TestCaseSource("testData")>]
+            let checkTests list = 
+                countEvenFilter list
 
+        (* Проверяем эквивалентность всех функций *)
         [<Test>]
-        let ``count for [1; 2; 3; -4; 0] should equal 3`` () =
-            countEvenFilter [1; 2; 3; -4; 0] |> should equal 3
+        let ``check equals`` () =
+            Check.Quick (fun list -> countEvenFilter list = countEvenFold list && 
+                                     countEvenFilter list = countEvenMap list)
+ 
 
         (* Task 3.2 tests *)
         [<Test>]
@@ -40,21 +53,21 @@ module Tests =
         (* Task 3.3 tests *)
         [<Test>]
         let ``calculate empty tree must throw ArgumentException`` () =
-            calculate Operation.Empty |> 
+            calculate None |> 
             should throw typeof<System.ArgumentException>
 
         [<Test>]
         let ``test for value`` () = 
-            calculate (Value(2)) |> should equal 2
+            calculate (Some(Value(2))) |> should equal 2
     
         [<Test>]
         let ``1/0 must throw DivideByZeroException`` ()=
-            calculate (Div(Value 1, Value 0)) |>
+            calculate (Some(Div(Value 1, Value 0))) |>
             should throw typeof<System.DivideByZeroException>
 
         [<Test>]
         let ``test for all operations (5 - 10) * 2 + 10 should equal 0`` () =
-            calculate (Add(Mult(Sub(Value 5, Value -10), Value 2), Value 10)) |> should equal 0
+            calculate (Some(Add(Mult(Sub(Value 5, Value -10), Value 2), Value 10))) |> should equal 0
 
         (* Task 3.4 test *)
         [<Test>]
